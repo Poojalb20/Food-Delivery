21c1680890ec158bfce5be0035d6d043a8803d77
Poojalb20 <1si23mc034@sit.ac.in>
2025-01-04 16:13:29 +0530

    nn

--- Backend/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const userRouter=express.Router();
-const {loginUser,registerUser}=require('../controllers/userController')
-
-console.log('check')
-userRouter.post('/register',registerUser)
-userRouter.post('/login',loginUser)
-
-module.exports=userRouter;
